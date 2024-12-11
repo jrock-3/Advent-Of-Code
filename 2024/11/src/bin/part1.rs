7e69754b277da4d74196df91a5bce1efde09f75a
Justin Liao <justin.liao3@gmail.com>
2024-12-10 22:54:55 -0800

    2024-11 Full Sol

---
+++ 2024/11/src/bin/part1.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let input = include_str!("../input.txt");
+    dbg!(process(input));
+}
+
+fn process(input: &str) -> String {
+    let mut nums = input
+        .trim()
+        .split(" ")
+        .map(|num| num.to_string())
+        .collect::<Vec<_>>();
+
+    for _ in 0..25 {
+        // dbg!(&nums);
+        nums = nums
+            .into_iter()
+            .flat_map(|num| {
+                if num == "0".to_string() {
+                    return vec!["1".to_string()].into_iter();
+                }
+                if num.len() % 2 == 0 {
+                    return vec![
+                        num[0..num.len() / 2].parse::<usize>().unwrap().to_string(),
+                        num[num.len() / 2..].parse::<usize>().unwrap().to_string(),
+                    ]
+                    .into_iter();
+                }
+                return vec![(num.parse::<usize>().unwrap() * 2024).to_string()].into_iter();
+            })
+            .collect::<Vec<_>>();
+    }
+
+    nums.len().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_in1() {
+        let input = "125 17";
+        assert_eq!("55312", process(input));
+    }
+}
---
+++ 2024/11/src/bin/part2.rs
@@ -0,0 +1,60 @@
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("../input.txt");
+    dbg!(process(input));
+}
+
+fn process(input: &str) -> String {
+    let mut nums = input
+        .trim()
+        .split(" ")
+        .map(|num| num.parse::<usize>().unwrap())
+        .counts();
+    // dbg!(&nums);
+
+    for _ in 0..75 {
+        for (num, cnt) in nums.clone() {
+            *nums.get_mut(&num).unwrap() -= cnt;
+            if num == 0 {
+                nums.entry(1)
+                    .and_modify(|e| {
+                        *e += cnt;
+                    })
+                    .or_insert(cnt);
+            } else if num.ilog10() % 2 == 1 {
+                let digits = 10usize.pow((num.ilog10() + 1) / 2);
+                nums.entry(num / digits)
+                    .and_modify(|e| {
+                        *e += cnt;
+                    })
+                    .or_insert(cnt);
+                nums.entry(num % digits)
+                    .and_modify(|e| {
+                        *e += cnt;
+                    })
+                    .or_insert(cnt);
+            } else {
+                nums.entry(num * 2024)
+                    .and_modify(|e| {
+                        *e += cnt;
+                    })
+                    .or_insert(cnt);
+            }
+        }
+        // dbg!(&nums);
+    }
+
+    nums.values().sum::<usize>().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_in1() {
+        let input = "125 17";
+        assert_eq!("", process(input));
+    }
+}
